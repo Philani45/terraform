bbe3194bafe7c0865f6cbc9ccd18c1d4f8c6e27e
Philani45 <Philani.ruwoko@gmail.com>
2025-10-10 18:45:44 +0000

    Added new Terraform modules for ELB and RDS, updated bastion config

--- c7-03-ec2instance-bastion.tf
+++ c7-03-ec2instance-bastion.tf
@@ -9,7 +9,6 @@ module "ec2_public" {
   monitoring             = true
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
-
-
-  tags = local.common_tags
+ tags                    = local.common_tags
+ associate_public_ip_address = true
 }
